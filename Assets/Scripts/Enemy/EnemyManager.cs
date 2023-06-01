d4da0a5a846ec7c42f1fd06d794f5e2f328fda06
Nathan Gagne <nathangagne.dev@gmail.com>
2023-06-01 13:56:52 -0500

    Increase enemy damage

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -6,7 +6,7 @@ public class EnemyManager : MonoBehaviour
 {
     private float enemyHP;
     [SerializeField] float maxHP;
-    private float ATTACK_POWER = 25;
+    private float ATTACK_POWER = 33;
 
     private bool isAlive = true;
     
